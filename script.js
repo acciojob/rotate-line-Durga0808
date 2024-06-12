4fd8c9ef5560b4359ab3f4251d1a5315af1022da
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-12 14:30:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let angle = 0;
+
+function rotateLine() {
+    angle = (angle + 2) % 360; // Increase angle by 2 degrees, keep within 0-359
+    document.getElementById('line').style.transform = `translate(-50%, -50%) rotate(${angle}deg)`;
+    requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
